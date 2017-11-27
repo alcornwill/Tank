16185386b461f6085a47189dbc46614584a27abe
Will Alcorn <alcornwill@gmail.com>
2017-11-27 13:44:35 +0000

    immediate mode

--- cube.c
+++ cube.c
@@ -13,12 +13,19 @@
 #define SCREEN_HEIGHT 480
 #define ASPECT_RATIO ((float)SCREEN_WIDTH / (float)SCREEN_HEIGHT)
 #define FOV 60
+#define NEAR 0.1f
+#define FAR 100.0f
 #define FPS 30
 #define TICKS_PER_SECOND ((float)1000 / (float)FPS)
 
+#define PI_OVER_180 0.0174532925199432957692369076849f
+#define _180_OVER_PI 57.2957795130823208767981548141f
 
-int init();
-int initGL();
+#define RADIANS(x) (x * PI_OVER_180)
+#define DEGREES(x) (x * _180_OVER_PI)
+
+void init();
+void initGL();
 void initTank();
 void update(float dt);
 void render();
@@ -28,12 +35,6 @@ void printShaderLog( GLuint shader );
 
 SDL_Window* gWindow = NULL;
 SDL_GLContext gContext;
-
-GLuint gProgramID = 0;
-GLint gVertexPos3DLocation = -1;
-GLint gMVPMatrixLocation = -1;
-GLuint gVBO = 0;
-GLuint gIBO = 0;
 
 unsigned char *keys;
 int quit = 0;
@@ -46,144 +47,50 @@ float gPlayerInputRot = 0;
 
 vec3_t gTankPosition;
 float gTankRotZ;
-mat4_t gTankModelMat;
 
-int init()
+void init()
 {
 	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		return 0;
-	}
+	SDL_Init( SDL_INIT_VIDEO );
 	
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 1 );
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY );
 
     //Create window
     gWindow = SDL_CreateWindow( "Tank Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
-    if( gWindow == NULL )
-    {
-        printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-        return 0;
-    }
 
     //Create context
     gContext = SDL_GL_CreateContext( gWindow );
-    if( gContext == NULL )
-    {
-        printf( "OpenGL context could not be created! SDL Error: %s\n", SDL_GetError() );
-        return 0;
-    }
     
     // Initialize GLEW
     glewExperimental = GL_TRUE; 
-    GLenum glewError = glewInit();
-    if( glewError != GLEW_OK )
-    {
-        printf( "Error initializing GLEW! %s\n", glewGetErrorString( glewError ) );
-    }
+    glewInit();
 
     //Initialize OpenGL
-    if( !initGL() )
-    {
-        printf( "Unable to initialize OpenGL!\n" );
-        return 0;
-    }
-
-	return 1;
+    initGL();
 }
 
-int initGL()
-{
-	gProgramID = glCreateProgram();
-    
-    // vertex shader
-	GLuint vertexShader = glCreateShader( GL_VERTEX_SHADER );
-	const GLchar* vertexShaderSource[] =
-	{
-		"#version 140\nin vec3 LVertexPos3D;\nuniform mat4 mvp;\n void main() { vec4 pos = vec4(LVertexPos3D, 1);\n pos = mvp * pos;\n gl_Position = pos; }"
-	};
-	glShaderSource( vertexShader, 1, vertexShaderSource, NULL );
-	glCompileShader( vertexShader );
-
-	// check for errors
-	GLint vShaderCompiled = GL_FALSE;
-	glGetShaderiv( vertexShader, GL_COMPILE_STATUS, &vShaderCompiled );
-	if( vShaderCompiled != GL_TRUE )
-	{
-		printf( "Unable to compile vertex shader %d!\n", vertexShader );
-		printShaderLog( vertexShader );
-        return 0;
-	}
-    
-    glAttachShader( gProgramID, vertexShader );
-
-    // fragment shader
-    GLuint fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
-    const GLchar* fragmentShaderSource[] =
-    {
-        "#version 140\nout vec4 LFragment; void main() { LFragment = vec4( 1.0, 1.0, 1.0, 1.0 ); }"
-    };
-    glShaderSource( fragmentShader, 1, fragmentShaderSource, NULL );
-    glCompileShader( fragmentShader );
-
-    // check for errors
-    GLint fShaderCompiled = GL_FALSE;
-    glGetShaderiv( fragmentShader, GL_COMPILE_STATUS, &fShaderCompiled );
-    if( fShaderCompiled != GL_TRUE )
-    {
-        printf( "Unable to compile fragment shader %d!\n", fragmentShader );
-        printShaderLog( fragmentShader );
-        return 0;
-    }
-    
-    glAttachShader( gProgramID, fragmentShader );
-
-    //Link program
-    glLinkProgram( gProgramID );
-
-    //Check for errors
-    GLint programSuccess = GL_TRUE;
-    glGetProgramiv( gProgramID, GL_LINK_STATUS, &programSuccess );
-    if( programSuccess != GL_TRUE )
-    {
-        printf( "Error linking program %d!\n", gProgramID );
-        printProgramLog( gProgramID );
-        return 0;
-    }
-        
-    //Get vertex attribute location
-    gVertexPos3DLocation = glGetAttribLocation( gProgramID, "LVertexPos3D" );
-    if( gVertexPos3DLocation == -1 )
-    {
-        printf( "LVertexPos3D is not a valid glsl program variable!\n" );
-        return 0;
-    }
-    
-    //Get model matrix location
-    gMVPMatrixLocation = glGetUniformLocation( gProgramID, "mvp" );
-    if( gMVPMatrixLocation == -1 )
-    {
-        printf( "mvp is not a valid glsl program variable!\n" );
-        return 0;
-    }
-            
-    //Initialize clear color
+void initGL()
+{  
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     glClearColor( 0.f, 0.f, 0.f, 1.f );
 
-    //Create VBO
-    glGenBuffers( 1, &gVBO );
-    glBindBuffer( GL_ARRAY_BUFFER, gVBO );
-    glBufferData( GL_ARRAY_BUFFER, VERTICES * sizeof(GLfloat), vertexData, GL_STATIC_DRAW );
-
-    //Create IBO
-    glGenBuffers( 1, &gIBO );
-    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, gIBO );
-    glBufferData( GL_ELEMENT_ARRAY_BUFFER, EDGES * sizeof(GLuint), edgeData, GL_STATIC_DRAW );
-	
-	return 1;
+    int w = SCREEN_WIDTH / 2;
+    int h = SCREEN_HEIGHT / 2;
+    
+    float fovy = RADIANS(FOV);
+    float tangent = tanf(fovy / 2.0f);
+    float height = NEAR * tangent;
+    float width = height * ASPECT_RATIO;
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-width, width, -height, height, NEAR, FAR);
+    
+    // view transform
+    glTranslatef(0, -1, -5);
+    glRotatef(-90, 1.0f, 0.0f, 0.0f);
 }
 
 void initTank()
@@ -227,54 +134,44 @@ void update(float dt)
     
     // apply velocity
     gTankPosition = v3_add(gTankPosition, velocity);
-    
-    
-    // update matrices
-    // projection matrix
-    mat4_t proj = m4_perspective(FOV, ASPECT_RATIO, 0.1f, 100.0f);
-    
-    // so the coordinate system is:
-    // -Y is up
-    // -Z is forward
-    // X is right
-    // ...
-    
-    // view matrix
-    mat4_t view = m4_translation(vec3(0, -1, -5));
-    view = m4_mul(view, m4_rotation_x(-M_PI / 2));
-    
-    // model matrix   
-    mat4_t model = m4_translation(gTankPosition);
-    model = m4_mul(model, m4_rotation_z(gTankRotZ));
-    
-    mat4_t mv = m4_mul(proj, view);
-    gTankModelMat = m4_mul(mv, model);
 }
 
 void render()
 {
 	glClear( GL_COLOR_BUFFER_BIT );
-    
-    glUseProgram( gProgramID );
-    glEnableVertexAttribArray( gVertexPos3DLocation );
-    
-    glUniformMatrix4fv(gMVPMatrixLocation, 1, GL_FALSE, &gTankModelMat);
-    
-    glBindBuffer( GL_ARRAY_BUFFER, gVBO );
-    glVertexAttribPointer( gVertexPos3DLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), NULL );
-    
-    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, gIBO );
-    glDrawElements( GL_LINES, EDGES, GL_UNSIGNED_INT, NULL );
 
-    glDisableVertexAttribArray( gVertexPos3DLocation );
-
-    glUseProgram( 0 );
+    glMatrixMode(GL_MODELVIEW);
+    
+    // tank
+    glLoadIdentity();
+    glTranslatef(gTankPosition.x, gTankPosition.y, gTankPosition.z);
+    glRotatef(DEGREES(gTankRotZ), 0.0f, 0.0f, 1.0f);
+    glBegin(GL_LINES);
+    for (int e = 0; e < EDGES / 2; ++e)
+    {
+        int edge = e * 2;
+        int v1 = edgeData[edge];
+        int v2 = edgeData[edge+1];
+        
+        float x1, y1, z1;
+        int vert1 = v1 * 3;
+        x1 = vertexData[vert1];
+        y1 = vertexData[vert1+1];
+        z1 = vertexData[vert1+2];
+        glVertex3f(x1, y1, z1);
+        
+        float x2, y2, z2;
+        int vert2 = v2 * 3;
+        x2 = vertexData[vert2];
+        y2 = vertexData[vert2+1];
+        z2 = vertexData[vert2+2];
+        glVertex3f(x2, y2, z2);
+    }
+    glEnd();
 }
 
 void close()
-{
-	glDeleteProgram( gProgramID );
-    
+{    
 	SDL_DestroyWindow( gWindow );
 	gWindow = NULL;
 
@@ -325,12 +222,7 @@ void printShaderLog( GLuint shader )
 
 int main(int argc, char *argv[])
 {    
-	if( !init() )
-    {
-		printf( "Failed to initialize!\n" );
-        close();
-        return 0;
-    }
+	init();
     
     initTank();
     keys = SDL_GetKeyboardState(NULL);
